94640c2f79056166e06b45b3fd38351777ff0d2c
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-08 18:13:36 -0400

    [test-fixture] Some theoretical test to make sure we have a template

--- test/html.lisp
+++ test/html.lisp
@@ -1,14 +1,6 @@
 (in-package :cl-undead-test)
 (in-suite main)
 
-(test can-read-html
-  (is nil))
-
-(test can-find-element
-  (is nil))
-
-(test can-find-child
-  (is nil))
-
-(test can-find-class
-  (is nil))
+(with-fixture html/simple ()
+  (test can-haz-html
+    (is (> 0 (length template-data)))))
