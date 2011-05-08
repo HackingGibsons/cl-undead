1afa65bf44f2a4c45fd9a8fe135756f516eb5999
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-08 18:07:51 -0400

    [test-fixture] No reason to remove it there

--- test/fixtures.lisp
+++ test/fixtures.lisp
@@ -1,6 +1,5 @@
 (in-package :cl-undead-test)
 
-(rem-fixture 'html/simple)
 (def-fixture html/simple
     (&key (template "test.simple.html") (dir "data"))
   (let* ((data-dir (asdf:system-relative-pathname :cl-undead-test
