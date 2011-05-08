b6cfaa9e5577c85472e186c33e62ace12c06ef8c
Sean Bryant <sean@vitrue.com>
2011-05-08 18:51:55 -0400

    [master] some tests to find elements in tree.

--- test/html.lisp
+++ test/html.lisp
@@ -3,3 +3,10 @@
 
 (test (can-haz-html :fixture html/simple)
   (is (> (length template-data) 0)))
+
+(test (can-find-root :fixture html/simple)
+  (is (find-in-tree parsed-data (node-named :html))))
+
+(test (can-find-children :fixture html/simple)
+  (is (eql 2 (length (find-in-tree parsed-data (node-named :span)))))
+  (is (eql 2 (length (find-in-tree parsed-data (node-named :span))))))
