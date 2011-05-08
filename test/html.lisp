658e763a558a9e4da5f3c15fe3c63bb5ebb825c3
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-08 13:45:10 -0400

    [master] Test additions

---
+++ test/html.lisp
@@ -0,0 +1,14 @@
+(in-package :cl-undead-test)
+(in-suite main)
+
+(test can-read-html
+  (is nil))
+
+(test can-find-element
+  (is nil))
+
+(test can-find-child
+  (is nil))
+
+(test can-find-class
+  (is nil))
