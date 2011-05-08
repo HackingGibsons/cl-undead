b7bc8e6e93ad4b24b473b366c0c950b583262885
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-08 12:43:06 -0400

    [master] Adding an API

--- cl-undead.lisp
+++ cl-undead.lisp
@@ -2,5 +2,9 @@
 
 (in-package #:cl-undead)
 
-
+(defun process-template (template &rest rules)
+  "Process the `template' according to the list of transformations
+given by `rules' in the form (((CSS-PATH :selector :description) TREE-TRANSFORMATION)
+                              ((CSS-PATH :AWESOME) LESS-AWESOME-TREE-TRANSFORMATION))"
+  :undefined)
 
