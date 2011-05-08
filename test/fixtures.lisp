1932e7a90b3bf72e1c1ce6d300d8d502374d37c0
Sean Bryant <sean@vitrue.com>
2011-05-08 18:51:30 -0400

    [master] add a parsed version of the html-string since it's be using it often.

--- test/fixtures.lisp
+++ test/fixtures.lisp
@@ -4,5 +4,7 @@
   (let* ((data-dir (asdf:system-relative-pathname :cl-undead-test
                                                  (make-pathname :directory `(:relative ,dir))))
          (template-file (make-pathname :name template :defaults data-dir))
-         (template-data (file-string template-file)))
+         (template-data (file-string template-file))
+         (parsed-data (chtml:parse template-data nil)))
+    (declare (ignorable data-dir template-file template-data parsed-data))
     (&body)))
