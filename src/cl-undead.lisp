2dacc73a5e19d41a9374b4f774e804f0721ec4b5
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-08 20:40:10 -0400

    [rules-sketch] Generalized removing an element by passing nil as the transform

--- src/cl-undead.lisp
+++ src/cl-undead.lisp
@@ -6,7 +6,19 @@
   "Process the `template' according to the list of transformations
 given by `rules' in the form (((CSS-PATH :selector :description) TREE-TRANSFORMATION)
                               ((CSS-PATH :AWESOME) LESS-AWESOME-TREE-TRANSFORMATION))"
-  (let ((parsed (chtml:parse template nil)))
+  (let ((parsed (chtml:parse template nil))
+        matches)
+    (dolist (rule rules)
+      (unless rule (return))
+      (destructuring-bind (path . proc) rule
+        (setf matches (find-in-tree parsed path))
+        (mapc #'(lambda (match)
+                  (if proc
+                      (funcall proc match)
+                      (let* ((parent (pt-parent match)) (siblings (pt-children parent)))
+                        (setf (pt-children parent)
+                              (remove-if (lambda (node) (eql node match)) siblings)))))
+              matches)))
     parsed))
 
 (defun node-named (name)
