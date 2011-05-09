377870520e327f168c3886b03307b8b6653be2aa
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-09 00:46:42 -0400

    [master] Fetching flatten, in case I want it

--- test/packages.lisp
+++ test/packages.lisp
@@ -1,6 +1,9 @@
 (defpackage #:cl-undead-test
   (:use :cl :fiveam :cl-undead)
   (:import-from :cl-undead
+                ;; Helpers
+                :flatten
+
                 ;; Internal
                 :find-in-tree
                   :make-named-p :make-id-p
