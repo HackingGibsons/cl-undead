baa3d4304e81057c865845242bf6c37c337fb3b7
Yaroslav Shirokov <slava@hackinggibsons.com>
2011-05-08 21:52:00 -0400

    [rules-sketch] Bring tests in line with refactor

--- test/html.lisp
+++ test/html.lisp
@@ -5,8 +5,8 @@
   (is (> (length template-data) 0)))
 
 (test (can-find-root :fixture html/simple)
-  (is (find-in-tree parsed-data (node-named :html))))
+  (is (find-in-tree parsed-data (make-named-p :html))))
 
 (test (can-find-children :fixture html/simple)
-  (is (eql 1 (length (find-in-tree parsed-data (node-named :body)))))
-  (is (eql 2 (length (find-in-tree parsed-data (node-named :span))))))
+  (is (eql 1 (length (find-in-tree parsed-data (make-named-p :body)))))
+  (is (eql 2 (length (find-in-tree parsed-data (make-named-p :span))))))
--- test/packages.lisp
+++ test/packages.lisp
@@ -1,3 +1,7 @@
 (defpackage #:cl-undead-test
   (:use :cl :fiveam :cl-undead)
-  (:import-from :cl-undead :find-in-tree :node-named :process-template))
+  (:import-from :cl-undead
+                ;; Internal
+                :find-in-tree :make-named-p
+                ;; API-ish
+                :process-template))
